37b506fc257699df5d86c6fc32aa28e1909c4911
Brayden Reimann <braydenreimann@LaceysMFall2013.home>
2024-12-19 22:18:25 -0500

    Fixed bugs in relation to button

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-
 const spanishTranslations = {
     introduction: "Me llamo",
-    title: "CS @ Purdue, aspirante a desarollador de software"
+    title: "CS @ Purdue, aspirante a desarollador de software",
+    button: "English"
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+    // Set the initial language to English
+    localStorage.setItem("lang", "en");
+
     // Set the language in localStorage when the language button is toggled
     document.getElementById("lang-toggle").addEventListener("click", () => {
-        localStorage.setItem("lang", "es");
-        console.log("this happened");
-        switchLang();
+        let lang = localStorage.getItem("lang");
+        if (lang === "en") {
+            localStorage.setItem("lang", "es");
+            lang = "es";
+        } else if (lang === "es") {
+            localStorage.setItem("lang", "en");
+            lang = "en";
+        }
+        switchLang(lang);
     });
 })
 
@@ -30,7 +39,13 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 })
 
-function switchLang() {
-    document.getElementById("intro").innerText = spanishTranslations.introduction;
-    document.getElementById("title").innerText = spanishTranslations.title;
+// Function for switching languages
+function switchLang(lang) {
+    if (lang === "en") {
+        location.reload();
+    } else if (lang === "es") {
+        document.getElementById("intro").innerText = spanishTranslations.introduction;
+        document.getElementById("title").innerText = spanishTranslations.title;
+        document.getElementById("lang-toggle").innerText = spanishTranslations.button;
+    }
 }
